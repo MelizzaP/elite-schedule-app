7f6064d63c81580630d80953955c4f12863706ff
Melissa Patterson <melissa.patterson.va@gmail.com>
2017-05-05 14:22:31 -0500

    add some simple navigation

--- src/pages/my-teams/my-teams.ts
+++ src/pages/my-teams/my-teams.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import  { Tournaments }  from '../pages';
+
 @IonicPage()
 @Component({
-  selector: 'page-my-teams',
-  templateUrl: 'my-teams.html',
+    selector: 'page-my-teams',
+    templateUrl: 'my-teams.html',
 })
 export class MyTeams {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(private nav: NavController) {
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MyTeams');
-  }
+    goToTournaments() {
+        this.nav.push(Tournaments);
+    }
 
 }
